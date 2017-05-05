8247f2335dbd6c7915914ba73c3b3023707037e4
Fran Oviamionayi <fran.ovia@gmail.com>
2017-05-05 13:21:11 +0200

    Adding Travis CI

--- setup.py
+++ setup.py
@@ -8,10 +8,8 @@ setup(
   author = 'Fran Oviamionayi',
   author_email = 'fran.ovia@gmail.com',
   url = 'https://github.com/fran-ovia/tabtree-py',
-  download_url = 'https://github.com/fran-ovia/tabtree-py/archive/0.0.1.tar.gz',
+  download_url = 'https://github.com/fran-ovia/tabtree-py/releases/download/0.0.1/tabtree-0.0.1.zip',
   keywords = ['tabbed-tree', 'parsing', 'tab', 'tree'],
-  tests_require=['pytest'],
-  test_suite = 'tests',
   classifiers = [
     'Programming Language :: Python',
     'Development Status :: 3 - Alpha',
