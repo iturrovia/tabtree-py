b729cb50ccd0964ccbc6aadcd25d56296f092ea9
Fran Oviamionayi <fran.ovia@gmail.com>
2017-05-05 16:17:26 +0200

    Changing release version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'tabtree',
   packages = ['tabtree'], # this must be the same as the name above
-  version = '0.1.b0',
+  version = '0.2.0',
   description = 'Parsing tabbed tree formatted text files into dict node trees',
   license='MIT License',
   author = 'Fran Oviamionayi',
   author_email = 'fran.ovia@gmail.com',
   url = 'https://github.com/fran-ovia/tabtree-py',
-  download_url = 'https://github.com/fran-ovia/tabtree-py/releases/download/0.1.b0/tabtree-0.1.b0.zip',
+  download_url = 'https://github.com/fran-ovia/tabtree-py/releases/download/0.2.0/tabtree-0.2.0.zip',
   keywords = ['tabbed-tree', 'parsing', 'tab', 'tree'],
   classifiers = [
     'Programming Language :: Python',
