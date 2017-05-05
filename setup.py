b805027a8e1a6653fc933a0207c9bd940d80131d
Fran Oviamionayi <fran.ovia@gmail.com>
2017-05-05 15:16:54 +0200

    Preparing 0.1.b0 release

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from distutils.core import setup
 setup(
   name = 'tabtree',
   packages = ['tabtree'], # this must be the same as the name above
-  version = '0.0.1',
+  version = '0.1.b0',
   description = 'Parsing tabbed tree formatted text files into dict node trees',
   license='MIT License',
   author = 'Fran Oviamionayi',
   author_email = 'fran.ovia@gmail.com',
   url = 'https://github.com/fran-ovia/tabtree-py',
-  download_url = 'https://github.com/fran-ovia/tabtree-py/releases/download/0.0.1/tabtree-0.0.1.zip',
+  download_url = 'https://github.com/fran-ovia/tabtree-py/releases/download/0.1.b0/tabtree-0.1.b0.zip',
   keywords = ['tabbed-tree', 'parsing', 'tab', 'tree'],
   classifiers = [
     'Programming Language :: Python',
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
     'Natural Language :: English',
-    'Environment :: Web Environment',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
--- tabtree/__init__.py
+++ tabtree/__init__.py
@@ -2,4 +2,4 @@
 Package providing a module to parse the content from tabbed tree files
 
 """
-__version__ = '0.0.1'
+__version__ = '0.1.b0'
